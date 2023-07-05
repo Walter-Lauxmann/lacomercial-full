ca7e124e11446805cb38807cf72c449f0f9f0d95
Walter Lauxmann <walterlauxmann@institutocedec.com>
2023-07-05 16:56:26 -0300

    Listado con API REST modificado

--- controladores/articulos.js
+++ controladores/articulos.js
@@ -1,7 +1,13 @@
-import {articulos} from '../modelos/articulos';
+import { obtenerArticulos } from "../modelos/articulos";
 
-const listado = document.getElementById('listado');
-for(let articulo of articulos) {
+document.addEventListener('DOMContentLoaded', () => {
+    cargarArticulos();
+})
+
+async function cargarArticulos() {
+  const articulos = await obtenerArticulos();
+  const listado = document.getElementById("listado");
+  for (let articulo of articulos) {
     listado.innerHTML += `
         <div class="col">
             <div class="card" style="width: 18rem;">
@@ -9,16 +15,14 @@ for(let articulo of articulos) {
                 <div class="card-body">
                     <h5 class="card-title"><span name="spancodigo">${articulo.codigo}</span> - <span name="spannombre">${articulo.nombre}</span></h5>
                     <p class="card-text">
-                        Procesador: ${articulo.descripcion.procesador}<br>
-                        Almacenamiento: ${articulo.descripcion.almacenamiento}<br>
-                        Cámaras: ${articulo.descripcion.camaras}<br>
-                        Pantalla: ${articulo.descripcion.pantalla}<br>
+                        ${articulo.descripcion}<br>
                     </p>
                     <h5>$ <span name="spanprecio">${articulo.precio}</span></h5>
                     <input class="form-control" type="number" value="0" min="0" max="30" name="inputcantidad" onchange="calcularPedido()">
                 </div>
             </div>
         </div>
-    `
+    `;
+  }
 }
 
